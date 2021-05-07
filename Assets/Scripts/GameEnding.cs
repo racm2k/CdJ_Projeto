ac5195ede09be89e319ae6bc7c88bd6dd38f8775
racm2k <58236873+racm2k@users.noreply.github.com>
2021-05-07 16:36:04 +0100

    Added Menu
    Created a main menu with Play, Options and Quit buttons.

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -9,10 +9,16 @@ public class GameEnding : MonoBehaviour
     public float displayImageDuration = 1f;
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+
+    public AudioSource exitAudio;
     public CanvasGroup caughtBackgroundImageCanvasGroup;
+
+    public AudioSource caughtAudio;
     bool m_IsPlayerAtExit;
     bool m_IsPlayerCaught;
     float m_Timer;
+
+    bool m_HasAudioPlayed;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player)
@@ -29,23 +35,27 @@ public class GameEnding : MonoBehaviour
     {
         if (m_IsPlayerAtExit)
         {
-            EndLevel(exitBackgroundImageCanvasGroup, false);
+            EndLevel(exitBackgroundImageCanvasGroup, false,exitAudio);
         }
         else if (m_IsPlayerCaught)
         {
-            EndLevel(caughtBackgroundImageCanvasGroup, true);
+            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
         }
     }
 
-    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
     {
+        if(!m_HasAudioPlayed){
+            audioSource.Play();
+            m_HasAudioPlayed=true;
+        }
         m_Timer += Time.deltaTime;
         imageCanvasGroup.alpha = m_Timer / fadeDuration;
         if (m_Timer > fadeDuration + displayImageDuration)
         {
             if (doRestart)
             {
-                SceneManager.LoadScene(0);
+                SceneManager.LoadScene(1);
             }
             else
             {
